48711ca553f48352efa63fe5b16ac6bfa63ab32f
Gabriel Perez <gabrielrperez.dev@gmail.com>
2024-03-08 04:28:08 -0500

    local storage methods

--- To-Do/app.js
+++ To-Do/app.js
@@ -80,7 +80,11 @@ openTaskFormBtn.addEventListener("click", () =>
 closeTaskFormBtn.addEventListener("click", () => {
   const formInputsContainValues =
     titleInput.value || dateInput.value || descriptionInput.value;
-  if (formInputsContainValues) {
+  const formInputValuesUpdated =
+    titleInput.value !== currentTask.title ||
+    dateInput.value !== currentTask.date ||
+    descriptionInput.value !== currentTask.description;
+  if (formInputsContainValues && formInputValuesUpdated) {
     confirmCloseDialog.showModal();
   } else {
     reset();
@@ -99,3 +103,16 @@ taskForm.addEventListener("submit", (e) => {
 
   addOrUpdateTask();
 });
+
+const myTaskArr = [
+  { task: "Walk the Dog", date: "22-04-2022" },
+  { task: "Read some books", date: "02-11-2023" },
+  { task: "Watch football", date: "10-08-2021" },
+];
+
+localStorage.setItem("data", JSON.stringify(myTaskArr));
+const getTaskArr = localStorage.getItem("data");
+console.log(getTaskArr);
+const getTaskArrObj = JSON.parse(localStorage.getItem("data"));
+console.log(getTaskArrObj);
+localStorage.clear();
