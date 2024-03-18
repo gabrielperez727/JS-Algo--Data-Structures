b6beefde8116dfbad4006b245e1faaa7bd24a25b
Gabriel Perez <gabrielrperez.dev@gmail.com>
2024-03-18 06:35:58 -0400

    selection sort algorithm

--- algorithmic-thinking/app.js
+++ algorithmic-thinking/app.js
@@ -4,7 +4,7 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
-  const sortedValues = bubbleSort(inputValues);
+  const sortedValues = insertionSort(inputValues);
   updateUI(sortedValues);
 };
 
@@ -20,13 +20,42 @@ const updateUI = (array = []) => {
 const bubbleSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
-      console.log(array, array[j], array[j + 1]);
       if (array[i] > array[j]) {
         const temp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = temp;
       }
     }
+  }
+  return array;
+};
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      console.log(array, array[j], array[minIndex]);
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+  return array;
+};
+
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const currValue = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > currValue) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currValue;
   }
   return array;
 };
