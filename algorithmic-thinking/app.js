3710552228ced63b13d3b97572737cf508d12fff
Gabriel Perez <gabrielrperez.dev@gmail.com>
2024-03-18 05:53:53 -0400

    bubble sort algorithm

--- algorithmic-thinking/app.js
+++ algorithmic-thinking/app.js
@@ -4,7 +4,8 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
-  updateUI(inputValues);
+  const sortedValues = bubbleSort(inputValues);
+  updateUI(sortedValues);
 };
 
 sortButton.addEventListener("click", sortInputArray);
@@ -14,4 +15,18 @@ const updateUI = (array = []) => {
     const outputValueNode = document.getElementById(`output-value-${i}`);
     outputValueNode.innerText = num;
   });
+};
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      console.log(array, array[j], array[j + 1]);
+      if (array[i] > array[j]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
 };
