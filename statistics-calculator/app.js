eedd52101edee7bfcdb230101567fe020e374cb1
Gabriel Perez <gabrielrperez.dev@gmail.com>
2024-03-19 05:10:15 -0400

    calculate function

---
+++ statistics-calculator/app.js
@@ -0,0 +1,11 @@
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array
+    .map((el) => {
+      Number(el);
+    })
+    .filter((el) => {
+      !isNaN(el);
+    });
+};
