fa6b44c86c26b0235f4ddfac583370698e8a04e1
Gabriel Perez <gabrielrperez.dev@gmail.com>
2024-03-22 05:43:45 -0400

    middle function

--- Spreadsheet/app.js
+++ Spreadsheet/app.js
@@ -33,3 +33,11 @@ const charRange = (start, end) =>
 const sum = (nums) => nums.reduce((acc, el) => acc + el);
 const isEven = (num) => (num % 2 === 0 ? true : false);
 const average = (nums) => sum(nums) / nums.length;
+const median = (nums) => {
+  const sorted = nums.slice().sort((a, b) => a - b);
+  const length = sorted.length;
+  const middle = length / 2 - 1;
+  return isEven(length)
+    ? average([sorted[middle], sorted[middle + 1]])
+    : sorted[Math.ceil(middle)];
+};
