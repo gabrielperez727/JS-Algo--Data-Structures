bf8f562b937dda4540680590b2ebb7f09a96f7b4
Gabriel Perez <gabrielrperez.dev@gmail.com>
2024-03-22 04:35:25 -0400

    window.onload and range function

---
+++ Spreadsheet/app.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+  const container = document.getElementById("container");
+  const createLabel = (name) => {
+    const label = document.createElement("div");
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
+  };
+};
+
+const range = (start, end) =>
+  Array(end - start + 1)
+    .fill(start)
+    .map((element, index) => element + index);
