347423aa179eb260e4b8f2c63a351f78b9157526
Gabriel Perez <gabrielrperez.dev@gmail.com>
2024-03-27 05:41:20 -0400

    regex deny lists

--- spam-filter/app.js
+++ spam-filter/app.js
@@ -14,4 +14,10 @@ checkMessageButton.addEventListener("click", () => {
   messageInput.value = "";
 });
 
-const isSpam = (msg) => false;
+const helpRegex = /please help|assist me/i;
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex];
+const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
+const stockRegex = /[s5][t7][o0]ck [a@4]l[e3]r[t7]/i;
+const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
+const dearRegex = /(?:^|\s)d[e3][a@4]r fr[i1|][e3]nd(?:$|\s)/i;
